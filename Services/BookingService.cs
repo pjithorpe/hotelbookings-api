b7461737b70cd8d359cef991c8d979778f8f5b72
Patrick Thorpe <pjithorpe@gmail.com>
2019-08-22 17:14:35 +0100

    Begin implementing Repository pattern structure.

---
+++ Services/BookingService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using HotelBookingsAPI.Models;
+using HotelBookingsAPI.Repositories;
+using HotelBookingsAPI.Services;
+
+namespace Services
+{
+    public class BookingService : IBookingService
+    {
+        private IBookingRepository bookingRepository;
+
+        public BookingService(IBookingRepository bookingRepository)
+        {
+            this.bookingRepository = bookingRepository;
+        }
+
+        public async Task<IEnumerable<Booking>> ListAsync()
+        {
+            return await bookingRepository.ListAsync();
+        }
+    }
+}
