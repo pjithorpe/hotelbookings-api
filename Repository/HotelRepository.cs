623661c519e3e6fcdd0646106e09cb536c415e5f
Patrick Thorpe <pjithorpe@gmail.com>
2019-08-23 13:42:56 +0100

    Add Repository base abstract class and implement hotel repository class.

---
+++ Repository/HotelRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HotelBookingsAPI;
+using HotelBookingsAPI.Models;
+
+namespace Repository
+{
+    public class HotelRepository : Repository
+    {
+        public HotelRepository(AppDbContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public async Task<IEnumerable<Hotel>> ListAsync()
+        {
+            return await dbContext.Hotels.ToListAsync();
+        }
+    }
+}
---
+++ Repository/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using HotelBookingsAPI;
+
+namespace Repository
+{
+    public abstract class Repository
+    {
+        protected AppDbContext dbContext;
+
+        public Repository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
