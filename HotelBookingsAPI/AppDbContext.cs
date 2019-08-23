4dee5ff8a8a307486d79af5d6a9d55c9e668e35e
Patrick Thorpe <pjithorpe@gmail.com>
2019-08-23 17:03:52 +0100

    Fix db context to use correct model name for Hotel table.

--- HotelBookingsAPI/AppDbContext.cs
+++ HotelBookingsAPI/AppDbContext.cs
@@ -11,16 +11,13 @@ namespace HotelBookingsAPI
     {
         public DbSet<Hotel> Hotels { get; set; }
 
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-            
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
-            builder.Entity<Hotel>().ToTable("dbo.Hotel");
+            builder.Entity<Hotel>().ToTable("Hotel");
             builder.Entity<Hotel>().HasKey(h => h.HotelID);
             builder.Entity<Hotel>().Property(h => h.HotelID).IsRequired().ValueGeneratedOnAdd();
             builder.Entity<Hotel>().Property(h => h.Name).IsRequired().HasMaxLength(200);
